5fa843d6091272c55178416c46fdcbc8853ff146
Andrew Lehman <aplehm@gmail.com>
2022-09-14 19:11:03 -0400

    Use twig to render pages

--- public/index.php
+++ public/index.php
@@ -3,13 +3,20 @@
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$twigLoader = new FilesystemLoader('./templates');
 
-$app->get('/', function(Request $request, Response $response, $args) {
-    $response->getBody()->write('Hello Joan!');
+$app->get('/', function(Request $request, Response $response, $args) use ($twigLoader) {
+    $template = new Environment($twigLoader);
+    $response->getBody()->write($template->render('home.twig', [
+        'title' => 'DISCORD API TEST',
+    ]));
+
     return $response;
 });
 
