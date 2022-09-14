5f6afed89df0e7f233f97b2970d9ed5f27972320
Andrew Lehman <aplehm@gmail.com>
2022-09-13 22:19:20 -0400

    remove slim psr7

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php declare(strict_types=1);
 
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use Slim\Factory\AppFactory;
-use Slim\Psr7\Request;
-use Slim\Psr7\Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function(Request $request, Response $response, $args) {
-    $response->getBody()->write('Hello world!');
+    $response->getBody()->write('Hello Joan!');
     return $response;
 });
 
